c32c6c68ebb1fc748d4b11832645a46da396e637
Valentino <vsanchez@stemdo.io>
2025-02-19 12:53:22 +0100

    ejercicio 5

--- soluciones/Ejercicio6/main.tf
+++ soluciones/Ejercicio6/main.tf
@@ -170,7 +170,6 @@ resource "ibm_is_lb_pool_member" "member1" {
   lb            = ibm_is_lb.load_balancer.id
   pool          = ibm_is_lb_pool.lb_pool.id
   port          = 80
-  target_id     = ibm_is_instance.vm_valentino_ej06_1.primary_network_interface[0].primary_ip.address
   weight        = 50
 }
 
@@ -178,7 +177,6 @@ resource "ibm_is_lb_pool_member" "member2" {
   lb            = ibm_is_lb.load_balancer.id
   pool          = ibm_is_lb_pool.lb_pool.id
   port          = 80
-  target_id     = ibm_is_instance.vm_valentino_ej06_2.primary_network_interface[0].primary_ip.address
   weight        = 50
 }
 
